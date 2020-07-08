1627696a39453a7e4edfc6b7eb230b83fa475903
bathtub-physicist <bathtub.earth@gmail.com>
2020-07-08 20:09:58 +0530

    Finaw finaw cowmmit i pwetty pwomise OWO

--- post.py
+++ post.py
@@ -1,7 +1,7 @@
-import json
+import json, random
 import time
-import demoji
 
+import demoji
 from selenium.webdriver import Chrome
 
 with open('./credentials.json') as f:
@@ -18,25 +18,38 @@ def submit_post(text):
     browser.get(url)
     print(browser.title)
     
-    time.sleep(5)
+    time.sleep(3)
     
     input_field = browser.find_element_by_tag_name("textarea")
-    
     text = demoji.replace(text)
-    
     input_field.send_keys(text)
     
-    time.sleep(2)
+    time.sleep(1)
     
     button = browser.find_element_by_css_selector("div[class='freebirdFormviewerViewNavigationButtons'] > div[role='button']")
-    
     button.click()
     
     time.sleep(1)
     
     
 posts = posts["posts"]
+random.shuffle(posts)
+
+total = len(posts)
+count = 0
 
 for post in posts:
     submit_post(post["selftext"])
+    count += 1
+    
     print("Done with {}".format(post["title"]))
+    print("Progress: {}/{}".format(count, total))
+    print("=" * 20)
+    
+    
+    
+    
+    
+    
+    
+
--- scrape.py
+++ scrape.py
@@ -20,12 +20,17 @@ print(subreddit.description)
 
 posts_dict = {"posts": []} # [{"title": , "url": , "selftext": }]
 
+count = 1
 for submission in subreddit.hot(limit=40):
+    print("No. {}, Post title: {}".format(count, submission.title))
+    
     posts_dict["posts"].append({
         "title": submission.title,
         "url": submission.url,
         "selftext": submission.selftext
     })
+    
+    count += 1
 
 with open('posts.json', 'w') as f:
     json.dump(posts_dict, f)
